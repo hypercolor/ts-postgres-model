399d4b242ff18f66e04f96a0a069abaf06b848fb
Andrew Aarestad <andrew@aa-engineering.com>
2018-04-25 09:22:09 -0500

    more exports

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@
 export { PostgresModel } from './src/PostgresModel'
 export {IScopeFactory} from './src/IScopeFactory'
 export {IUser} from './src/IUser'
-export {Scope, IKnexObject, IScopeableObject} from './src/Scope'
+export {Scope, IKnexObject, IScopeableObject, ScopeAction} from './src/Scope'
 export {Column} from './src/Column'
